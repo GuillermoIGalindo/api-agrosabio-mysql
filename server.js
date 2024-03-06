926a04eb7f6e6351042c1d65749d80a0aa595dc2
Guillermo Galindo <guillegalindo45@gmail.com>
2024-03-06 10:21:23 -0600

    api agrosabio en mysql aun sin modificar para el proyecto

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//create roles of the static form
+const db = require("./src/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+// routes
+require('./src/routes/auth.routes')(app);
+require('./src/routes/user.routes')(app);
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+  })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenido a la aplicación" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo por el puerto ${PORT}.`);
+});
